73fcf48683e96fb6f3be102cbe7f942cea096b3e
Vikash Pathak <pathakvikash821@gmail.com>
2023-09-11 21:26:56 +0530

    added rating

--- frontend/src/components/product/index.tsx
+++ frontend/src/components/product/index.tsx
@@ -10,15 +10,20 @@ import { baseUrl } from '@/constant';
 import { useSelector } from 'react-redux';
 
 const Product = ({ product }: any) => {
-  const { name, category, price, image_url } = product;
+  const { name, category, price, image_url, rating } = product;
+  const stars = Array.from(Array(rating), (_, index) => (
+    <i key={index} className='fas fa-star'></i>
+  ));
   return (
-    <div className=' w-full rounded overflow-hidden shadow-lg mx-2 my-2 hover:shadow-2xl transition duration-300 transform hover:scale-105'>
+    <div className='w-full rounded overflow-hidden shadow-lg mx-2 my-2 hover:shadow-2xl transition duration-300 transform hover:scale-105'>
       <img src={image_url} alt={name} className='w-full h-48 object-cover' />
       <Link href={`/products/${product.id}`}>
         <div className='px-6 py-4'>
-          <div className='font-bold text-gray-600 text-xl mb-2'>{name}</div>
+          <div className='font-bold text-gray-600 text-sm mb-2'>{name}</div>
           <p className='text-gray-600 text-base mb-2'>{category}</p>
-          <p className='text-gray-700 text-base'>{price}</p>
+          <p className='text-gray-700 text-base'>₹{price}</p>
+
+          {stars}
         </div>
       </Link>
     </div>
@@ -53,7 +58,7 @@ const ProductList = () => {
   return (
     <div className='flex flex-wrap justify-center'>
       {filterProducts?.map((product: any) => (
-        <div key={product.id} className='flex w-[350px] '>
+        <div key={product.id} className='flex w-[350px] h-fit '>
           <Product product={product} />
         </div>
       ))}
