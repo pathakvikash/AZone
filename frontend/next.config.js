3b9bf186fd8665a781f20ff62b795b79cdcfa31b
Vikash Pathak <pathakvikash821@gmail.com>
2023-09-27 03:23:50 +0530

    chore(frontend): assest & img config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'links.papareact.com',
+      'images-na.ssl-images-amazon.com',
+      'images-eu.ssl-images-amazon.com',
+      'fakestoreapi.com',
+      'm.media-amazon.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
